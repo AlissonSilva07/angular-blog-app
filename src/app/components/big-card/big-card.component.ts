1f96a7b5632e7817f834409c3b89d277aad68d21
Alisson Rego <alissondroid07@gmail.com>
2023-09-10 19:26:15 -0300

    feat: organização de componentes

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
-
+  @Input() bigCardTitle: string = '';
+  @Input() bigCardSubtitle: string = '';
+  @Input() bigCardImage: string = '';
+  @Input() bigCardAuthor: string = '';
+  @Input() bigCardDate: string = '';
 }
